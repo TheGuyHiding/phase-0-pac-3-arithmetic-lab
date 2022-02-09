8102638f6227f3f2b5a01209b4c821ccffd84621
Luis <luis9609@gmail.com>
2022-02-08 23:28:12 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+number = 10;
+
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n){
+    parseInt("2", 10);
+    return n;
+}
+
+function preserveDecimal(n){
+    parseFloat("2.222");
+    return n;
+}
